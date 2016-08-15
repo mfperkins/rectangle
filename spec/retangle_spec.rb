fc6ff278b992f2d6c0fe9d65859c7fa4e0182866
Matthew Perkins <mfperkins@gmail.com>
2016-08-15 21:49:35 +0100

    got rspec test working

--- spec/retangle_spec.rb
+++ spec/retangle_spec.rb
@@ -2,6 +2,6 @@ require 'rectangle'
 
 describe 'print_rectangle' do
   it 'returns "|  |" on the 2nd and 3rd line when I input 5 as a length and 3 as a height' do
-    expect(print_rectangle(5,3)).to eq '|   |'
+    expect(print_rectangle(5,3)).to eq ["-----", ["|   |", "|   |", "|   |"], "-----"]
   end
 end
